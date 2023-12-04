337369d840754d64c42920edbbad5b6bc62ae09e
tellybah2208 <thiernobah2208@gmail.com>
2023-12-04 17:47:47 +0100

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Telly BAH on 04/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
